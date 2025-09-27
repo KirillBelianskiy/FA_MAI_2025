b89613704fc6286756deee6d02cacb8d9199838a
KirillBelianskiy <belianskiykirill@gmail.com>
2025-09-27 23:16:44 +0300

    a small fix

--- lab_08/include/functions.h
+++ lab_08/include/functions.h
@@ -5,7 +5,6 @@
 
 int parse_input(const int argc, const char** argv, int *base, int* nums);
 
-int fromNto10(const char* num, const int n);
 int from10toN(int num, const int n, char* output);
 
 int max(const int* nums, const int size);
